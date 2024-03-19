85dd80c56f464f6c02b1ef88b823d579b81f052d
kim4ky <kimaky121209@gmail.com>
2024-03-19 16:53:41 +0900

    投稿保存機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,26 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
     @posts = Post.order("created_at DESC")
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:game_mode_id, :content).merge(user_id: current_user.id)
   end
 end
