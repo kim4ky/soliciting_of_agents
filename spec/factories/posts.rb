a12f4b4ee42f3673fbfea7698be46a62f3e4fb51
kim4ky <kimaky121209@gmail.com>
2024-03-20 14:34:24 +0900

    postモデルの単体テストコード実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :post do
-    
+    game_mode_id { 2 }
+    content { Faker::Lorem.sentence }
+    association :user
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+  describe '新規募集投稿' do
+    context '投稿できる場合' do
+      it '必要事項を全て過不足なく入力すると投稿できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '投稿できない場合' do
+      it 'game_mode_idが選択されていないと投稿できない' do
+        @post.game_mode_id = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("ゲームモードを選択してください")
+      end
+
+      it 'contentが入力されていないと投稿できない' do
+        @post.content = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("投稿内容を入力してください")
+      end
+
+      it 'userが紐付いていなければ投稿できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("ユーザーを入力してください")
+      end
+    end
+  end
 end
