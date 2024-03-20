82809d857bae82111a5c0efe07bea4dc299ecae8
kim4ky <kimaky121209@gmail.com>
2024-03-20 12:25:56 +0900

    エラー文の設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ApplicationRecord
 
   belongs_to :user
 
-  validates :game_mode_id, numericality: { other_than: 1 }
+  validates :game_mode_id, numericality: { other_than: 1, message: "を選択してください" }
   validates :content, presence: true
 end
