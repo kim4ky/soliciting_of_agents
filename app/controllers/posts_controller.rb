64cd8de6495b2885110d903254f58d8d9808c29f
kim4ky <kimaky121209@gmail.com>
2024-03-21 11:49:33 +0900

    投稿編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.order("created_at DESC")
@@ -19,12 +20,29 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+    unless current_user == @post.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:game_mode_id, :content).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
