03459656df641f8a27fe13b0d7ca6b3cc78a2d97
kim4ky <kimaky121209@gmail.com>
2024-03-21 17:46:16 +0900

    commentモデル・commentsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_18_064208) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_21_082452) do
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", charset: "utf8", force: :cascade do |t|
     t.integer "game_mode_id", null: false
     t.text "content", null: false
@@ -33,5 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_18_064208) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
