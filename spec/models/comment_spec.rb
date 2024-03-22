585d189fb5c01a95da132f9327cca8b77534f589
kim4ky <kimaky121209@gmail.com>
2024-03-22 16:08:55 +0900

    commentモデルの単体テストコード実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  describe '#create' do
+    context 'コメント投稿できる場合' do
+      it 'コメントが入力されていれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context '投稿できない場合' do
+      it 'コメントが空では投稿できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Textを入力してください")
+      end
+
+      it 'ユーザーが紐付いていなければコメントできない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Userを入力してください")
+      end
+
+      it "投稿が紐付いていなければコメントできない" do
+        @comment.post = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Postを入力してください")
+      end
+    end
+  end
 end
